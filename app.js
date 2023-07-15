590df53db547e585a9523f5382e8f9835993697a
daeun118 <ceromi04@naver.com>
2023-07-15 18:08:16 +0900

    make calculator

---
+++ app.js
@@ -0,0 +1,20 @@
+const calculator={
+    add:function(a,b){
+        console.log("The answer is "+(a+b));
+    },
+    minus:function(a,b){
+        console.log("The answer is "+(a-b));
+    },
+    multiple:function(a,b){
+        console.log("The answer is "+(a*b));
+    },
+    divide:function(a,b){
+        console.log("The answer is "+(a/b))
+    },
+};
+
+
+calculator.add(2,3);
+calculator.minus(2,3);
+calculator.multiple(2,3);
+calculator.divide(2,3);
