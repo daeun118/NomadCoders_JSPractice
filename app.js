2c523232d8dce4b9d96be98dcac56435117b5fc6
daeun118 <ceromi04@naver.com>
2023-07-16 19:20:30 +0900

    Use a toggle!

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const h1=document.querySelector("div.hello:first-child h1");
 
 
 function handleTitleClick(){
-    const clickedClass="clicked";
-    if (h1.classList.contains(clickedClass)){ //classList가 clicked를 포함하고 있는지만을 확인
-        h1.classList.remove(clickedClass);
-    } else {
-        h1.classList.add(clickedClass);
-    }
+    h1.classList.toggle("clicked");
+    //toggle은 h1의 classList에 clickedclass가 이미 있는지 확인해서
+    //만약 있다면, toggle이 clicked를 제거.
+    //만약 h1의 classList에 clicked가 존재하지 않는다면, 
+    //toggle은 clicked를 classList에 추가함.
 }
 
 h1.addEventListener("click", handleTitleClick);
