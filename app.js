0ddc4db6d199c0553c34d9fa72cec2739ce4e8b2
daeun118 <ceromi04@naver.com>
2023-07-16 17:55:05 +0900

    Event listening and color change

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
-const title=document.querySelector("div.hello:first-child h1");
+const h1=document.querySelector("div.hello:first-child h1");
 
-console.dir(title);
 
 function handleTitleClick(){
-    console.log("title was clicked!");
-    title.style.color="blue";
+    const clickedClass="clicked";
+    if (h1.className === clickedClass){
+        h1.className="";
+    } else {
+        h1.className=clickedClass
+    }
 }
 
-function handleMouseenter(){
-    console.log("Mouse is here!");
-    title.innerText="Mouse is here!";
-}
+h1.addEventListener("click", handleTitleClick);
 
-function handleNouseleave(){
-    console.log("Mouse is gone!");
-    title.innerText="Mouse is gone!";
-}
-
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseenter);
-title.addEventListener("mouseleave", handleNouseleave);
