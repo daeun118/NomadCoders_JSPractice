fbb58b0edd14657583b31004a5ff82ab451cfa7f
daeun118 <ceromi04@naver.com>
2023-07-16 14:38:59 +0900

    listen click event.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const title=document.querySelector(".hello h1");
-//이전에 getElementsByClassName() 함수를 쓸 때는 hello만 씀.
-//JS에서 우리가 class name을 넘겨준다는 것을 알기 때문.
-//하지만 querySelector에서는 hello가 class name이라는 것과 그 안의 h1을 명시해야 함.
+const title=document.querySelector("div.hello:first-child h1");
 
-console.log(title);
+
+function handleTitleClick(){
+    console.log("title was clicked!");
+}
+
+title.addEventListener("click", handleTitleClick);
+
+
