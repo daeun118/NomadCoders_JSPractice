67869c084be055e3463008883b30c90bf9e239c9
daeun118 <ceromi04@naver.com>
2023-07-16 14:07:49 +0900

    find elements by querySelector

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-const age=parseInt(prompt("How old are you?"));
+const title=document.querySelector(".hello h1");
+//이전에 getElementsByClassName() 함수를 쓸 때는 hello만 씀.
+//JS에서 우리가 class name을 넘겨준다는 것을 알기 때문.
+//하지만 querySelector에서는 hello가 class name이라는 것과 그 안의 h1을 명시해야 함.
 
-console.log();
-
-if (isNaN(age)){
-    console.log("Please write a number")
-} else{
-    console.log("Thank you for writing your age!")
-}
+console.log(title);
