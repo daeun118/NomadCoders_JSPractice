c42797e66f7c8d203368b5e1a632d159d1c27b76
daeun118 <ceromi04@naver.com>
2023-07-16 17:00:09 +0900

    Event Listening

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const title=document.querySelector("div.hello:first-child h1");
 
+console.dir(title);
 
 function handleTitleClick(){
     console.log("title was clicked!");
     title.style.color="blue";
 }
 
+function handleMouseenter(){
+    console.log("Mouse is here!");
+    title.innerText="Mouse is here!";
+}
+
+function handleNouseleave(){
+    console.log("Mouse is gone!");
+    title.innerText="Mouse is gone!";
+}
+
 title.addEventListener("click", handleTitleClick);
-
-
+title.addEventListener("mouseenter", handleMouseenter);
+title.addEventListener("mouseleave", handleNouseleave);
