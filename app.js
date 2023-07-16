75ddcd8cb4a2d98153c2448b4c1bc58b818eb61b
daeun118 <ceromi04@naver.com>
2023-07-16 18:02:03 +0900

    Using classList, not className

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const h1=document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
     const clickedClass="clicked";
-    if (h1.className === clickedClass){
-        h1.className="";
+    if (h1.classList.contains(clickedClass)){ //classList가 clicked를 포함하고 있는지만을 확인
+        h1.classList.remove(clickedClass);
     } else {
-        h1.className=clickedClass
+        h1.classList.add(clickedClass);
     }
 }
 
