ec5f685c37ffd680eccea799d41f31ee130316bc
daeun118 <ceromi04@naver.com>
2023-07-16 10:06:01 +0900

    JS condition basic

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
-const calculator={
-    add:function(a,b){
-        console.log("The answer is "+(a+b));
-    },
-    minus:function(a,b){
-        console.log("The answer is "+(a-b));
-    },
-    multiple:function(a,b){
-        console.log("The answer is "+(a*b));
-    },
-    divide:function(a,b){
-        console.log("The answer is "+(a/b))
-    },
-};
+const age=parseInt(prompt("How old are you?"));
 
+console.log();
 
-calculator.add(2,3);
-calculator.minus(2,3);
-calculator.multiple(2,3);
-calculator.divide(2,3);
+if (isNaN(age)){
+    console.log("Please write a number")
+} else{
+    console.log("Thank you for writing your age!")
+}
